fdeeda85744f969869b4c7f864c330321dd33012
nishant <nishantkudale04@gmail.com>
2024-12-03 16:32:04 +0530

    message function to print hello world five times for basic understanding of recursion

---
+++ 6recursion/R1_message.java
@@ -0,0 +1,25 @@
+public class R1_message {
+    public static void main(String[] args) {
+        message();
+    }
+    static void message(){
+        System.out.println("hello world");
+        message1();
+    }
+    static void message1(){
+        System.out.println("hello world");
+        message2();
+    }
+    static void message2(){
+        System.out.println("hello world");
+        message3();
+    }
+    static void message3(){
+        System.out.println("hello world");
+        message4();
+    }
+    static void message4(){
+        System.out.println("hello world");
+    }
+
+}
