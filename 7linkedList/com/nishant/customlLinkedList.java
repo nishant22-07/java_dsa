0f8976c32ca2a3a11335f8bf1aa3ce2e7633e48f
nishant <nishantkudale04@gmail.com>
2025-01-22 18:32:48 +0530

    customlLinkedList

---
+++ 7linkedList/com/nishant/customlLinkedList.java
@@ -0,0 +1,22 @@
+package com.nishant;
+
+
+public class customlLinkedList {
+    private node head;
+    private node tail;
+    private int size;
+    private class node {
+        private int value;
+        private node next;
+
+        public node(int value) {
+            this.value = value;
+        }
+
+        public node(int value, node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+}
