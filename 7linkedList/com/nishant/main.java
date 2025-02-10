f1baeed338f45fd893b20ba18d287f78abfe74d1
nishant <nishantkudale04@gmail.com>
2025-02-10 08:13:14 +0530

    skip 'A' char in string

---
+++ 7linkedList/com/nishant/main.java
@@ -0,0 +1,25 @@
+package com.nishant;
+
+import java.util.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList();
+        list.add(12);
+        list.add(15);
+        list.add(5);
+//        list.addFirst(25);
+//        list.addLast(45);
+        System.out.println(list);
+        System.out.println( list.get(2));
+
+
+//        list.insert(45);
+//        list.insert(12);
+//        list.insert(78);
+//        list.insertAtStart(25);
+//        list.insertAt(2, 55);
+//        list.deleteAt(2);
+//        list.show();
+    }
+}
