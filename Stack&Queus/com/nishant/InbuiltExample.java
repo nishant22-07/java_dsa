bcbfe1211ad87e434b2a968e6cedaea92870920e
nishant <nishantkudale04@gmail.com>
2025-04-30 18:24:25 +0530

    Stack push and pop operation

---
+++ Stack&Queus/com/nishant/InbuiltExample.java
@@ -0,0 +1,26 @@
+package com.nishant;
+
+
+import java.util.Stack;
+
+public class InbuiltExample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(22);
+        stack.push(07);
+        stack.push(2004);
+        stack.push(27);
+        stack.push(07);
+        stack.push(2004);
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+
+
+}
