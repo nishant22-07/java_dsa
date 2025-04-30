042807e7657d417c175bcc12fa3c88509cd2e44e
nishant <nishantkudale04@gmail.com>
2025-04-30 18:53:12 +0530

    stack, queue and deque

--- Stack&Queus/com/nishant/InbuiltExample.java
+++ Stack&Queus/com/nishant/InbuiltExample.java
@@ -1,24 +1,45 @@
 package com.nishant;
 
 
-import java.util.Stack;
+import java.util.*;
 
 public class InbuiltExample {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(22);
-        stack.push(7);
-        stack.push(2004);
-        stack.push(27);
-        stack.push(7);
-        stack.push(2004);
-        System.out.println(stack);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(22);
+//        stack.push(7);
+//        stack.push(2004);
+//        stack.push(27);
+//        stack.push(7);
+//        stack.push(2004);
+//        System.out.println(stack);
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+
+//        Queue<Integer> queue = new LinkedList<>();
+//        queue.add(22);
+//        queue.add(7);
+//        queue.add(2004);
+//        queue.add(27);
+//        queue.add(7);
+//        queue.add(2004);
+//        System.out.println(queue);
+//        System.out.println(queue.peek());
+//        System.out.println(queue.remove());
+//        System.out.println(queue);
+
+//
+//        Deque<Integer> deque = new ArrayDeque<>();
+//        deque.add(22);
+//        deque.addFirst(26);
+//        deque.addLast(22);
+//        System.out.println(deque.removeFirst());
+
+
 
     }
 
