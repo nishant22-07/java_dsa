4d14504070a94b538992afd8d5ab6a7d908e5dd7
nishant <nishantkudale04@gmail.com>
2026-03-17 23:59:18 +0530

    add hourglass pattern implementation and XML migration state files

---
+++ 5patterns/hourglass.java
@@ -0,0 +1,36 @@
+public class hourglass {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i-1; j++) {
+                System.out.print(" ");
+
+            }
+            for (int k = 1; k <=n-i+1; k++) {
+                System.out.print("*");
+
+            }
+            for (int k = 1; k <=n-i+1; k++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+        }
+         for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n-i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int k = 1; k <=i; k++) {
+                System.out.print("*");
+
+            }
+            for (int k = 1; k <=i; k++) {
+                 System.out.print("*");
+
+            }
+            System.out.println();
+         }
+    }
+}
